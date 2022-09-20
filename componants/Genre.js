c72aeb7f4fac04200e26ae95a5ae77c6898809e8
xive30 <deflo59@gmail.com>
2022-09-20 13:43:01 +0200

    fetch genre

--- componants/Genre.js
+++ componants/Genre.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState, UseEffect } from 'react'
+import Styles from '../styles/genre.module.css'
 
 export default function Genre() {
 
@@ -8,20 +9,25 @@ export default function Genre() {
     useEffect(() => {
 
         fetch("https://api.rawg.io/api/genres?key=1ffa16216d324d469708c437d40d38ca")
-        .then(response => response.json())
-        .then(data => {
-            setGenreJeu(data.results)
-            console.log(data.results);
-        })
+            .then(response => response.json())
+            .then(data => {
+                setGenreJeu(data.results)
+                console.log(data.results);
+            })
     }, [])
-  return (
-    <>
-        {genreJeu.map((genre, index) => (
-            <div key= {index}>
-                <p>{genre.name}</p>
+    return (
+        <>
+            <div className={ Styles.wrapper }>
+                {genreJeu.map((genre, index) => (
+                    <div key={index}>
+                        <img className={Styles.imgGenre} src={genre.image_background} alt={genre.name} />
+                        <div>
+                        <h2>{genre.name}</h2>
+                        </div>
+                    </div>
+
+                ))}
             </div>
-            
-        ))}
-    </>
-  )
+        </>
+    )
 }
